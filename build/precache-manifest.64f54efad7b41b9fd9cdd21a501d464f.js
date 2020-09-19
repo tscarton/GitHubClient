b2bb1934a9e62a594a04949623a20ef9f359466f
tberto <tberto@br.ibm.com>
2020-09-18 23:48:17 -0300

    initial version

---
+++ build/precache-manifest.64f54efad7b41b9fd9cdd21a501d464f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247a1fcc1fd83783738e8fa76f2253f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bac3688e5b0c705c77a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d9a4a8655189e0b081fb",
+    "url": "/static/css/main.352b127f.chunk.css"
+  },
+  {
+    "revision": "7bac3688e5b0c705c77a",
+    "url": "/static/js/2.8f196ef2.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.8f196ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9a4a8655189e0b081fb",
+    "url": "/static/js/main.a2f057e7.chunk.js"
+  },
+  {
+    "revision": "b0ac4c09e3f7f2d978e6",
+    "url": "/static/js/runtime-main.80a1a3cc.js"
+  }
+]);
